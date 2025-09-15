44d0b56ddbb8d9412b732bfe12a3cc4a065a4a2d
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-09-15 10:47:03 +0100

    Successfully installed react application

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FirstComponent from "./components/FirstComponent";
+
+function App() {
+  return (
+    <>
+      <h2>WELCOME TO MY FIRST REACT PROJECT</h2>
+      <FirstComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+const FirstComponent = () => {
+   return (
+    <div>
+        This is my frist component buil with vite react
+    </div>
+   )
+}
+
+export default FirstComponent
