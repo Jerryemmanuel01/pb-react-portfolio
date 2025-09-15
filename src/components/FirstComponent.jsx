0e1b877b1095eaec5a99facbf7d646538c69b626
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-09-15 11:10:14 +0100

    A paragraph was added

--- src/components/FirstComponent.jsx
+++ src/components/FirstComponent.jsx
@@ -1,9 +1,13 @@
 const FirstComponent = () => {
-   return (
+  return (
     <div>
-        This is my frist component buil with vite react
+      This is my frist component buil with vite react
+      <p>
+        This change was made significantly to show how you can push a new change
+        made in your codebase
+      </p>
     </div>
-   )
-}
+  );
+};
 
-export default FirstComponent
+export default FirstComponent;
